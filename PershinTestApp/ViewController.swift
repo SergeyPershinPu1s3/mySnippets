8c74ae715462e9a2d01ad4395aa6503f69f16012
Sergey Pershin <pershinpu1s3@gmail.com>
2021-04-07 15:13:46 +0300

    project commit

---
+++ PershinTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PershinTestApp
+//
+//  Created by Sergey Pershin on 07.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
